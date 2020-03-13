973b887699edcf4f6f96b97e090761f705ee5039
Daniel Amyakar <dan.rockit@gmail.com>
2020-03-13 19:52:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,9 +11,10 @@ def long_planeteer_calls(hamper)   # code an argument here
   hamper.any? {|wrd| wrd.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cup)        # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cup.include? (cheese_types)
 end
 
 
