332141bcfc7302c196267321963a488fd0aa5871
Daniel Amyakar <dan.rockit@gmail.com>
2020-03-13 19:42:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(box)        # code an argument here
+  box.each_with_index { |nm, ndx| puts "#{ndx + 1}. #{nm}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(bowl)  # code an argument here
+  new_bowl = bowl.collect {|el| (el.upcase + "!")}
+  return new_bowl
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(hamper)   # code an argument here
+  
 end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+
+
+
+
+
+
+
+
